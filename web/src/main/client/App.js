b7ec078dc54c9fccd41293733b3e3e2cb6f357fe
cxpqwvtj <cxpqwvtj@gmail.com>
2018-12-24 07:08:12 +0900

    material-uiのサンプルコード入れた

--- web/src/main/client/App.js
+++ web/src/main/client/App.js
@@ -1,9 +1,41 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { withStyles } from '@material-ui/core/styles'
+import FormControl from '@material-ui/core/FormControl'
+import MenuItem from '@material-ui/core/MenuItem'
+import InputLabel from '@material-ui/core/InputLabel'
+import Select from '@material-ui/core/Select'
+
 import logo from './logo.svg'
 import './App.css'
 
+const styles = theme => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  formControl: {
+    margin: theme.spacing.unit,
+    minWidth: 120,
+  },
+  selectEmpty: {
+    marginTop: theme.spacing.unit * 2,
+  },
+})
+
 class App extends Component {
+  static propTypes = {
+    classes: PropTypes.object.isRequired,
+  }
+
+  state = {
+    age: '',
+    name: 'hai',
+    labelWidth: 0,
+  }
+
   render() {
+    const { classes } = this.props
     return (
       <div className="App">
         <header className="App-header">
@@ -13,9 +45,27 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <FormControl className={classes.formControl}>
+          <InputLabel htmlFor="age-simple">Age</InputLabel>
+          <Select
+            value={this.state.age}
+            onChange={this.handleChange}
+            inputProps={{
+              name: 'age',
+              id: 'age-simple',
+            }}
+          >
+            <MenuItem value="">
+              <em>None</em>
+            </MenuItem>
+            <MenuItem value={10}>Ten</MenuItem>
+            <MenuItem value={20}>Twenty</MenuItem>
+            <MenuItem value={30}>Thirty</MenuItem>
+          </Select>
+        </FormControl>
       </div>
     )
   }
 }
 
-export default App
+export default withStyles(styles)(App)
